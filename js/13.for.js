e45773147c3590dac5720cb3c95779cd9b6ebc42
booldook <booldook@gmail.com>
2021-08-01 11:31:14 +0900

    ...

---
+++ js/13.for.js
@@ -0,0 +1,19 @@
+// 배열 - Array
+var hongkd = {
+	name: '홍길동',
+	birth: '2010-11-15',
+	gender: 'Man'
+}
+var hongks = {
+	name: '홍길순',
+	birth: '2010-05-15',
+	gender: 'Woman'
+}
+
+var students = ['홍길동', '홍길순', '홍길만'];
+console.log(students);
+students[1] = '홍길희';
+console.log(students);
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[2]);
