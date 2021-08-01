6bb96f561045a423f69dc16bf397cb1ed668e111
booldook <booldook@gmail.com>
2021-08-01 11:48:11 +0900

    prds

--- js/13.for.js
+++ js/13.for.js
@@ -17,3 +17,83 @@ console.log(students);
 console.log(students[0]);
 console.log(students[1]);
 console.log(students[2]);
+
+var fruits = ['banana', 'apple'];
+var arr = ['A', 11, function() {}, {name: '홍길동'}, fruits];
+console.log(arr);
+
+var students = [{
+	name: '홍길동',
+	birth: '2010-11-15',
+	gender: 'Man'
+}, {
+	name: '홍길순',
+	birth: '2010-05-15',
+	gender: 'Woman'
+}];
+console.log(students);
+
+var prds = [
+	{
+		title: 'Good Products 01',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p1.jpg',
+		price: '25.00'
+	}, {
+		title: 'Good Products 02',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p2.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 03',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p3.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 04',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p4.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 05',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p5.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 06',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p6.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 07',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p7.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 08',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p8.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 09',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p9.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 10',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p10.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 11',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p11.jpg',
+		price: '28.00'
+	}, {
+		title: 'Good Products 12',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti molestiae natus alias harum vero est delectus iure qui? Accusantium aspernatur saepe dolore neque cum incidunt fugit distinctio, culpa beatae.',
+		src: '../img/p12.jpg',
+		price: '28.00'
+	}, 
+]
+
