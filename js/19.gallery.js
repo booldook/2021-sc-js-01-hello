e2edf8955dc62cd8360d0192b380d0bd327d4bb4
booldook <booldook@gmail.com>
2021-08-08 10:24:25 +0900

    19.gallery

---
+++ js/19.gallery.js
@@ -0,0 +1,32 @@
+// document.getElementById("wrapper").style.border = '1px solid red';
+// document.getElementsByClassName("thumb")[0].style.border = '1px solid blue';
+// document.getElementsByClassName("thumb")[1].style.border = '1px solid blue';
+// document.getElementsByTagName("ul")[0].style.border = '1px solid green';
+
+// for(var i=0; i<document.getElementsByClassName('thumb').length; i++) {
+// 	document.getElementsByClassName("thumb")[i].style.border = '1px solid blue';
+// }
+
+// $('#wrapper').css('border', '1px solid red');
+// $(".thumb").css('border', '1px solid blue');
+// $("ul").eq(0).css('border', '1px solid green');
+
+// console.log( $('.wrapper') );
+// console.log( $('.wrapper')[0] );
+// console.log( document.getElementById("wrapper") );
+// console.log( $(document.getElementById("wrapper")) );
+// console.log( $($($('.wrapper')[0])[0])[0] );
+
+// console.log($('#wrapper'));
+
+$('#wrapper')[0].style.border = '10px solid red';
+
+/*
+$('#wrapper').addClass()
+$('#wrapper').removeClass()
+$('#wrapper').toggleClass()
+$('#wrapper').animate()
+$('#wrapper').show()
+$('#wrapper').hide()
+$('#wrapper').css()
+*/
