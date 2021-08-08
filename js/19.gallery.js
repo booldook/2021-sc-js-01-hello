0f9900044909b1a1f3b169777cdecb925af59655
booldook <booldook@gmail.com>
2021-08-08 12:00:53 +0900

    19.gallery

--- js/19.gallery.js
+++ js/19.gallery.js
@@ -12,8 +12,8 @@ console.log(border);
 // console.log(border.border-radius); // error
 console.log(border['border-radius']); // success
 
-var el = $('.img-wrap').css('border', '1px solid red'); // Setter
-console.log(el);
+var $el = $('.img-wrap').css('border', '1px solid red'); // Setter
+console.log($el);
 
 var obj = {
 	name: '홍길동',
@@ -21,3 +21,36 @@ var obj = {
 }
 console.log(obj.age);
 console.log(obj['age']);
+
+
+console.log( $('.thumb').attr('class') );
+// $('.thumb').eq(0).attr('id', 'thumb0');
+// $('.thumb').eq(1).attr('id', 'thumb1');
+
+document.getElementsByClassName('thumb')[0].id = 'thumb0';
+document.querySelectorAll('.thumb')[1].id = 'thumb1';
+
+/** 
+* ? 자바스크립트란?
+*
+* ! 1. DOM접근
+* ? getElementByID() / getElementsByClassName() / getElementsByTagName()
+* ? querySelector() / querySelectorAll()
+* ? $('CSS접근제어자')
+*
+* ! 2. DOM속성
+* ? document.querySelector('.wrapper').id
+* ? document.querySelector('.wrapper').id = 'thumb';
+* ? document.querySelector('.wrapper img').src;
+* ? document.querySelector('.wrapper img').src = '../img/p1.jpg';
+* ? $('.wrapper').attr('id');
+* ? $('.wrapper').attr('id', 'thumb');
+* ? $('.wrapper').attr('src');
+* ? $('.wrapper').attr('src', '..img/p1.jpg');
+*
+* ! 3. CSS제어
+*	? window.getComputedStyle(DOM).getPropertyValue('border')
+*	? document.querySelector('.wrapper img').style.border = '1px solid red';
+*	? $('.wrapper').css('border');
+*	? $('.wrapper').css('border', '1px solid green');
+*/
