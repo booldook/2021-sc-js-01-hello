d6a04cf7b711cbd4f5bc645f475d300c403ee88d
booldook <booldook@gmail.com>
2021-08-15 10:24:40 +0900

    21.slide.html

--- js/21.slide.js
+++ js/21.slide.js
@@ -1,8 +1,29 @@
 $('.fade-type .slide').hide();
 $('.fade-type .slide').eq(0).show();
 var idx = 0;
-var interval = window.setInterval(function() {
+var interval = window.setInterval(onInterval, 3000);
+$('.fade-type .bt-prev').click(onPrev);
+$('.fade-type .bt-next').click(onNext);
+$('.fade-type .pager').click(onPager);
+function onInterval() {
 	idx = (idx === 4) ? 0 : idx + 1;
+	chgSlide();
+	chgPager();
+}
+function onPrev() {
+
+}
+function onNext() {
+
+}
+function onPager() {
+	
+}
+function chgSlide() {
 	$('.fade-type .slide').hide();
 	$('.fade-type .slide').eq(idx).show();
-}, 3000);
+}
+function chgPager() {
+	$('.fade-type .pager').removeClass('active');
+	$('.fade-type .pager').eq(idx).addClass('active');
+}
