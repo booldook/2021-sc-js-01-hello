b91c40e29f6cdacf5443f8a81de66604b9b0263a
booldook <booldook@gmail.com>
2021-08-15 11:22:31 +0900

    22.slide

--- js/21.slide.js
+++ js/21.slide.js
@@ -1,6 +1,7 @@
 /************** global init *************/
-var idx = 0;
-var interval;
+var idx = 0;		// slide index
+var interval;		// setInterval index
+var lastIdx = $('.fade-type .slide').length - 1;
 
 /************** function init *************/
 function chgSlide() {
@@ -13,27 +14,36 @@ function chgPager() {
 }
 
 /************** event Callback *************/
-function onInterval() {
-	idx = (idx === 4) ? 0 : idx + 1;
+function onPrev() {
+	idx = (idx === 0) ? lastIdx : idx - 1;
 	chgSlide();
 	chgPager();
 }
-function onPrev() {
-	
-}
 function onNext() {
-	
+	idx = (idx === lastIdx) ? 0 : idx + 1;
+	chgSlide();
+	chgPager();
 }
 function onPager() {
-	
+	idx = $(this).index();
+	chgSlide();
+	chgPager();
+}
+function onEnter() {
+	clearInterval(interval);
+}
+function onLeave() {
+	interval = setInterval(onNext, 3000);
 }
 
 /************** event init *************/
-interval = window.setInterval(onInterval, 3000);
+interval = setInterval(onNext, 3000);
 $('.fade-type .bt-prev').click(onPrev);
 $('.fade-type .bt-next').click(onNext);
 $('.fade-type .pager').click(onPager);
+$('.fade-type').mouseenter(onEnter);
+$('.fade-type').mouseleave(onLeave);
 
 /************** start *************/
 $('.fade-type .slide').hide();
-$('.fade-type .slide').eq(0).show();
+$('.fade-type .slide').eq(idx).show();
---
+++ js/22.slide.js
@@ -0,0 +1,14 @@
+/* 
+- fade slide
+1. pager, bt-prev, bt-next, interval
+2. idx, interval, depth
+*/
+/************** global init *************/
+
+/************* function init ************/
+
+/************* event callback ***********/
+
+/*************** event init *************/
+
+/*************** start init *************/
