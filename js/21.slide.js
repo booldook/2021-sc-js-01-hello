71520fba63212416e61248857b4f9fa7e0e75f45
booldook <booldook@gmail.com>
2021-08-15 10:39:06 +0900

    21.slide.js 구조화

--- js/21.slide.js
+++ js/21.slide.js
@@ -1,24 +1,8 @@
-$('.fade-type .slide').hide();
-$('.fade-type .slide').eq(0).show();
+/************** global init *************/
 var idx = 0;
-var interval = window.setInterval(onInterval, 3000);
-$('.fade-type .bt-prev').click(onPrev);
-$('.fade-type .bt-next').click(onNext);
-$('.fade-type .pager').click(onPager);
-function onInterval() {
-	idx = (idx === 4) ? 0 : idx + 1;
-	chgSlide();
-	chgPager();
-}
-function onPrev() {
+var interval;
 
-}
-function onNext() {
-
-}
-function onPager() {
-	
-}
+/************** function init *************/
 function chgSlide() {
 	$('.fade-type .slide').hide();
 	$('.fade-type .slide').eq(idx).show();
@@ -27,3 +11,29 @@ function chgPager() {
 	$('.fade-type .pager').removeClass('active');
 	$('.fade-type .pager').eq(idx).addClass('active');
 }
+
+/************** event Callback *************/
+function onInterval() {
+	idx = (idx === 4) ? 0 : idx + 1;
+	chgSlide();
+	chgPager();
+}
+function onPrev() {
+	
+}
+function onNext() {
+	
+}
+function onPager() {
+	
+}
+
+/************** event init *************/
+interval = window.setInterval(onInterval, 3000);
+$('.fade-type .bt-prev').click(onPrev);
+$('.fade-type .bt-next').click(onNext);
+$('.fade-type .pager').click(onPager);
+
+/************** start *************/
+$('.fade-type .slide').hide();
+$('.fade-type .slide').eq(0).show();
